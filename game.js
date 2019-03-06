510349c9203317f8f34434eb15c2407c4309408a
Nouri T <zorisse@gmail.com>
2019-03-06 22:25:59 +0100

    intro : ok , button start moche functionnalité ok

--- game.js
+++ game.js
@@ -4,11 +4,37 @@
 const images = [
     { src: './images/OTfytjA.jpg', x: [275, 287], y: [690, 710] },
     { src: './images/fort.jpg', x: [245, 255], y: [745, 755] },
-    { src: '', x: [275, 287], y: [670, 700] }
+    { src: '', x: [275, 287], y: [670, 700] },
+    { src: '', x: [275, 287], y: [670, 700] },
+
 ]
 
-// start Button 
+// intro
+$('#start').hide();
+movingWaldo();
+setTimeout(myStopFunction, 5000);
+setTimeout(finaleIntroCanva, 5001);
+setTimeout(() => $('#start').show()
+    , 5001
+)
 
-coordonnee(275, 690);
 
-setInterval(introCanvas, 1000);
+
+// start Button game 
+$('#start').click(() => {
+    // choix de l'image 
+    let image = images[0];
+    // hide canvas intro 
+    $('#introCanvas').hide();
+    $('#start').hide();
+    // start the game 
+    $('#image').show();
+    $('#image').attr('src', image.src)
+    // help 
+
+    coordonnee(image.x[0], image.y[0])
+
+
+})
+
+
--- introCanva.js
+++ introCanva.js
@@ -1,32 +1,55 @@
 // draw canva on the iamge
-$('img').hide();
+//
 
 const imageIntro = [
     { name: 'bouqin', src: './images/intro/bouquin.jpg' },
     { name: 'droite', src: './images/intro/droiteDroite.jpg' },
     { name: 'gauche', src: './images/intro/gaucheCanne.jpg' },
-    { name: 'salut', src: './images/intro/salut.jpeg' }
-
-
+    { name: 'salut', src: './images/intro/salut.jpeg' },
+    { name: 'where is waldo', src: 'images/intro/whereiswaldo.png' },
 ]
+
+
+let id1;
+let id2;
 
 var ctx = document.getElementById('introCanvas').getContext('2d');
 
+
+//function 
 function clearCanvas() {
     ctx.clearRect(0, 0, 800, 400);
 }
-
 function introCanvas() {
     let img = new Image();
     let numRandom = Math.floor(Math.random() * imageIntro.length)
-
     img.src = imageIntro[numRandom].src
     img.onload = () => {
         ctx.drawImage(img, Math.floor(Math.random() * 800), Math.floor(Math.random() * 400), 100, 100);
     };
     clearCanvas();
 }
+function movingWaldo() {
+    id1 = setInterval(introCanvas, 350);
+    id2 = setInterval(introCanvas, 350);
+}
+function myStopFunction() {
+    clearInterval(id1);
+    clearInterval(id2);
 
+}
+
+
+
+function finaleIntroCanva() {
+    clearCanvas();
+    let img = new Image();
+    img.src = imageIntro[4].src
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, 800, 400);
+    }
+
+}
 
 
 
